207499068a5761ba38b733fc54a42b079ab97a51
Cbrieuc <couillerot.brieuc@gmail.com>
2016-11-02 11:45:11 -0400

    phptest

---
+++ index2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <title>BRIEUC COUILLEROT</title>
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<?php include("header.html");?>
+
+</body>
+</html>
